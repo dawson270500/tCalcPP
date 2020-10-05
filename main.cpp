a0a1bc56347c71a54b3d433b5c9f93f6652dd96c
dawson270500 <51200862+dawson270500@users.noreply.github.com>
2020-10-05 23:29:36 +0100

    V1.4 Latest

--- main.cpp
+++ main.cpp
@@ -1,14 +1,64 @@
 #include <iostream>
 #include <string>
-
+#include <math.h>
+#include <stdlib.h>
+#include <time.h>
 using namespace std;
 
-string doCalc(string Input, float mem, string lastAnswer){//Gets the string and coverts into something that is usable by the program, then works that out
+/*
+	tCalc v1.4
+	Written in C++ by Bailey Dawson dawson270500@gmail.com
+	Orginally written in Python, V1.0 to V1.2
+
+	A basic terminal calculator
+	Report on bugs and code suggestions on the github repo page
+
+
+	Code reuse allowed, but given credit.
+*/
+
+string lastAnswer = "0.0";
+string mem = "0.0";
+#define pi 3.14159265;
+
+string doCalc(string Input){//Gets the string and coverts into something that is usable by the program, then works that out
 	
+	if(Input[3] == '('){
+		string num;
+		for(int i = 4; i < Input.length(); i++){
+			if(Input[i] == ')'){
+				break;
+			} else if(isdigit(Input[i])){//Constructing numbers
+				num = num + Input[i];
+			} else if(Input[i] == '.'){
+				num = num + Input[i];
+			} else if(Input[i] == 'p'){
+				num = pi;
+			} else if(Input[i] == 'r'){
+				num = rand();
+			} else if(Input[i] == 'm'){
+				num = mem;
+			} else {
+				return "Error 2.5 | Invalid input";
+			}
+		}
+		float num1 = stof(num)* pi;
+		num1 = num1 / 180;
+		if(Input[0] == 's'){
+			return to_string(sinf(num1));
+		}
+		if(Input[0] == 't'){
+			return to_string(tanf(num1));
+		}
+		if(Input[0] == 'c'){
+			return to_string(cosf(num1));
+		}
+	}
+
 	if(!isdigit(Input[0])){
 		int op;
 
-		if(Input[0] == '+'){
+		if(Input[0] == '+'){//Finding operator
 			op = 1;
 		} else if(Input[0] == '-'){
 			op = 2;
@@ -17,23 +67,27 @@ string doCalc(string Input, float mem, string lastAnswer){//Gets the string and 
 		} else if(Input[0] == '/'){
 			op = 4;
 		} else{
-			return "Error | Invalid input";
+			return "Error 2.4 | Invalid input";
 		}
 
 		string num;
 		for(int i = 1; i < Input.length(); i++){
 			if(isdigit(Input[i])){//Constructing numbers
 				num = num + Input[i];
-
 			} else if(Input[i] == '.'){
 				num = num + Input[i];
-			} else {
-				return "Error | Invalid input";
+			} else if(Input[i] == 'p'){
+				num = pi;
+			} else if(Input[i] == 'r'){
+				num = rand();
+			} else if(Input[i] == 'm'){
+				num = mem;
+			}else {
+				return "Error 2.3 | Invalid input";
 			}
 		}
 
-
-		switch(op){
+		switch(op){//Final calculations
 			case 1:
 				return to_string(stof(lastAnswer) + stof(num, NULL));
 			case 2:
@@ -50,7 +104,6 @@ string doCalc(string Input, float mem, string lastAnswer){//Gets the string and 
 	string num2 = "";
 	int op = 0;
 
-
 	for(int i =0; i < Input.length(); i++){//Loop through input
 		if(isdigit(Input[i])){//Constructing numbers
 			if(num1Done){
@@ -66,6 +119,24 @@ string doCalc(string Input, float mem, string lastAnswer){//Gets the string and 
 					continue;
 				}
 				num1 = num1 + Input[i];continue;
+			} else if(Input[i] == 'p'){
+				if(num1Done){
+					num2 = pi;
+					continue;
+				}
+				num1 = pi;continue;
+			} else if(Input[i] == 'r'){
+				if(num1Done){
+					num2 = rand();
+					continue;
+				}
+				num1 = rand();continue;
+			} else if(Input[i] == 'm'){
+				if(num1Done){
+					num2 = mem;
+					continue;
+				}
+				num1 = mem;continue;
 			} else if(Input[i] == '+'){
 				op = 1;
 			} else if(Input[i] == '-'){
@@ -75,7 +146,7 @@ string doCalc(string Input, float mem, string lastAnswer){//Gets the string and 
 			} else if(Input[i] == '/'){
 				op = 4;
 			}else{
-				return "Error | Invalid input";
+				return "Error 2.1 | Invalid input";
 			}
 			num1Done = true;
 		}
@@ -96,38 +167,39 @@ string doCalc(string Input, float mem, string lastAnswer){//Gets the string and 
 			out = to_string(stof(num1, NULL) / stof(num2, NULL));
 			break;
 		default:
-			out = "Error | Invalid Input";
+			out = "Error 2.2 | Invalid Input";
 	}
 
 	return out;
 }
 
 int main(){
-	cout << "-- tCalc V1.3 -- Programmed by Bailey Dawson --\n-- Type ':e' to exit | ':h' for help--\n";//Not a plug
-	float mem;
-	string lastAnswer;
+	srand (time(NULL));
+	cout << "-- tCalc V1.4 -- Programmed by Bailey Dawson --\n-- Type ':e' to exit | ':h' for help --\n";//Not a plug
 
 	while (true){//Main loop
 		string userInput;
 		cout << "> ";
 		cin >> userInput;
-
 		if(userInput == ":e"){//Exit program
 			break;
 		} else if(userInput == ":h"){//Help
-			cout << "Will add soon";
+			cout << "You can only do one operation at once\nSupported Operators: All operations involing letters are case sensetive\n\t- Plus \t\t'<num1>+<num2>'\n\t- Minus \t'<num1>-<num2>'\n\t- Mutliply \t'<num1>*<num2>'\n\t- Divide \t'<num1>/<num2>'\n\t- Sine \t\t'sin(<num>)'\n\t- Cosine \t'cos(<num>)'\n\t- Tangent \t'tan(<num>)'\n";
+			cout << "Using Memory:\n\tJust enter m after getting the answer you want in Memory\n\tTo access memory put m instead of a number in a calcuation.\n";
+			cout << "Using 'constants':\n\tPi can be used in calcuation by typing 'p' instead of a number\n\tTo have a random number in a calculation type 'r' instead of a number\n";
 			continue;
 		} else if(userInput == "m"){
-			mem = stof(lastAnswer);
+			mem = lastAnswer;
+			continue;
 		} else{//Proper input parsing
-			string ans = doCalc(userInput, mem, lastAnswer);
+			string ans = doCalc(userInput);
 			if(ans[0] != 'E'){
 				lastAnswer = ans;
 			}
 			cout << ans + "\n";
 			continue;
 		}
-		cout << "Error | Intial if Statements";
+		cout << "Error 1.1 | Intial if Statements";
 	}
 
 	return 0;
